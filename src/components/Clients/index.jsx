c3319322929d422326b857b3b8fe96026d13b390
BenNiks1 <ikke.trodde@gmail.com>
2021-02-16 16:51:02 +0300

    clients section

--- src/components/Clients/index.jsx
+++ src/components/Clients/index.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Clients = () => {
-  return
-}
+const Clients = ({ clients }) => {
+  return (
+    <section className="clients">
+      <div className="clients__inner container">
+        <h2 className="clients__title">our clients</h2>
+        <ul className="clients__list ">
+          {clients.map((client) => (
+            <li className="list__item">
+              <img src={`/${client.img}.png`} alt="" />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
-export default Clients
+export default Clients;
